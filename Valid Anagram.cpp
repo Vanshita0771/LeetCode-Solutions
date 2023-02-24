d50c2fb773ffa10a504548101a51422df9462d4a
Vanshita Kakkar <78303804+Vanshita0771@users.noreply.github.com>
2023-02-24 20:46:39 +0530

    Create Valid Anagram.cpp

---
+++ Valid Anagram.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+class Solution {
+
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t) return true;
+        return false;
+    }
+};
